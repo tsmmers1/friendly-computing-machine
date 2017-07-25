ca44cc7eca67adcaabb1bb323c249e2771daa72d
tsmmers1 <tsmmers1@memphis.edu>
2017-07-25 13:29:34 -0500

    Construct math files

---
+++ friendly_computing_machine/math.py
@@ -0,0 +1,15 @@
+"""
+A small set of functions for doing match operations.
+"""
+
+# Write a function named add that adds two values
+
+def add(arg1, arg2):
+    return arg1 + arg2
+
+def mult(arg1, arg2):
+    return arg1 * arg2
+
+def div(arg1, arg2):
+    return arg1 / arg2
+
