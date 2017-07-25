368754e38c482210521e287d40d7f0b99118696f
tsmmers1 <tsmmers1@memphis.edu>
2017-07-25 14:05:57 -0500

    Include testing for multiplicative functions

--- README.md
+++ README.md
@@ -3,4 +3,8 @@
 """
 How to install the testing:
 pip install -e .
+
+
+How to run the test itself:
+py.test -v
 """
--- tests/test_math.py
+++ tests/test_math.py
@@ -8,3 +8,7 @@ import pytest
 def test_add():
     assert fcm.math.add(5,2) == 7
     assert fcm.math.add(2,5) == 7
+
+def test_mult():
+    assert fcm.math.mult(5,2) == 10
+    assert fcm.math.mult(2,5) == 10
